acef0c18acf18e1b7a5b99d4b26b782f12012d28
Miftahur Rizki <62686080+mrizki10@users.noreply.github.com>
2021-05-16 20:58:23 +0700

    File

---
+++ Layanan.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- My fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+
+    <!-- My css -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>JhonyDesign</title>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <div class="container">
+        <a class="navbar-brand" href="#">JhonyDesign.com</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link active">Layanan</a>
+            <a class="nav-item nav-link" href="kontak.php">Kontak</a>
+            <a class="nav-item nav-link" href="about.php">About</a>
+            <a class="nav-item tombol btn btn-primary" href="https://api.whatsapp.com/send?phone=08381076527">Join Us</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid layanan">
+      <div class="container">
+        <h1 class="display-4">Layanan Kami</h1>
+      </div>
+    </div>
+
+    <!-- Akhir Jumbotron -->
+    <div class="container">
+    <!-- Panel -->
+    <section class="Layanan ">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card">
+                    <img src="img/1.jpeg" class="card-img-bottom" alt="card1">
+                    <div class="card-body">
+                    <h5 class="card-title"><u>Sampul Buku</u></h5>
+                    <p class="card-text">Melayani pembuatan sampul buku, online maupun offline akan kami layani</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/2.jpeg" class="card-img-bottom" alt="card2">
+                    <div class="card-body">
+                    <h5 class="card-title"><u>Undangan</u></h5>
+                    <p class="card-text">Membuat undangan sesuai yang kamu mau dan kamu pilih</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/4.jpg" class="card-img-bottom" alt="card3">
+                    <div class="card-body">
+                    <h5 class="card-title"><u>Banner</u></h5>
+                    <p class="card-text">Membuat Banner sesuai yang kamu mau dan dengan design yang kamu sukai</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Panel -->
+
+      <!-- Footer -->
+      <div class="row footer">
+        <div class="col text-center">
+          <p>2020 All Rights Reserved by Miftahur Rizki & Anto</p>
+        </div>
+      </div>
+      <!-- Akhir Footer -->
+
+    </div>
+    <!-- Akhir Container -->
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- My fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+
+    <!-- My css -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>JhonyDesign</title>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <div class="container">
+        <a class="navbar-brand">JhonyDesign.com</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="Layanan.php">Layanan</a>
+            <a class="nav-item nav-link" href="kontak.php">Kontak</a>
+            <a class="nav-item nav-link" href="about.php">About</a>
+            <a class="nav-item  tombol btn btn-primary" href="https://api.whatsapp.com/send?phone=08381076527">Join Us</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <!-- Jumbptron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4"><span> JhonyDesign.com </span> Buka Design <br>Siap <span>Melayani</span> Anda</h1>
+        <a href="https://api.whatsapp.com/send?phone=08381076527" class="btn btn-primary tombol">Hubungi Kami</a>
+      </div>
+    </div>
+    <!-- Akhir Jumbotron -->
+
+    <!-- Container -->
+    <div class="container">
+      <!-- Info Panel -->
+      <div class="row justify-content-center">
+        <div class="col-10 info-panel">
+          <div class="row"> 
+            <div class="col-lg">
+              <img src="img/birthday-boy.png" alt="terpercaya" class="float-left">
+              <h4>Terpercaya</h4>
+              <p>Dijamin terpercaya dan amanah</p>
+            </div>  
+            <div class="col-lg">
+              <img src="img/clock.png" alt="24_jam" class="float-left">
+              <h4>24 Jam</h4>
+              <p>Melayani anda setiap saat</p>
+            </div>
+            <div class="col-lg">
+              <img src="img/engineering.png" alt="cepat" class="float-left">
+              <h4>Cepat</h4>
+              <p>Melayani anda dengan cepat</p>
+            </div>
+          
+          </div>
+        </div>
+      </div>
+      <!-- Akhir Panel -->
+
+      <!-- Working space -->
+      <div class="row workingspace">
+        <div class="col-lg-6">
+          <img src="img/7.jpg" alt="workingspace" class="img-fluid">
+        </div> 
+        <div class="col-lg-6">
+          <h2>Buka Design dengan <span>JhonyDesign.com</span></h2>
+          <p>Design dengan penuh ceria dan cerita dibalik maknanya</p>
+          <a href="galleri.php" class="btn btn-primary tombol">Gallery</a>
+        </div>
+      </div>
+      <!-- Akhir Working space -->
+
+      <!-- Testimonial -->
+      <section class="testimonial">
+        <div class="row justify-content-center">
+          <div class="col-lg-8">
+            <h5>
+              "Designnya menarik dan tertarik untuk menjadi pelanggan setia"
+            </h5>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-lg-6 justify-content-center d-flex">
+            <figure class="figure">
+              <img src="img/b.jpg" class="figure-img img-fluid rounded-circle" alt="testi1">
+            </figure>
+            <figure class="figure">
+              <img src="img/d.jpg" class="figure-img img-fluid rounded-circle utama" alt="testi1">
+              <figcaption class="figure-caption">
+                <h6>Riansah</h6>
+                <p>Aktivis</p>
+              </figcaption>
+            </figure>
+            <figure class="figure">
+              <img src="img/c (1).jpg" class="figure-img img-fluid rounded-circle" alt="testi1">
+            </figure>
+          </div>
+        </div>
+      </section>
+      <!-- Akhir testimonial -->
+
+      <!-- Footer -->
+      <div class="row footer">
+        <div class="col text-center">
+          <p>2020 All Rights Reserved by Miftahur Rizki & <a href="php/login.php" style="color: #acacac;">Anto</a> </p>
+        </div>
+      </div>
+      <!-- Akhir Footer -->
+
+    </div>
+    <!-- Akhir Container -->
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
